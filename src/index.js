4a0d76147e17596f2d9c082fc08bbb67255e4f62
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-04 16:53:19 +0200

    build: setup cache-control

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ const { send } = require('micri')
 
 const { GITHUB_USER } = process.env
 
+const ONE_DAY_SECONDS = 86400
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('content-type', 'application/json; charset=utf-8')
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.setHeader('Cache-Control', `max-age=${ONE_DAY_SECONDS}, public`)
 
   const repos = await githubRepositories(GITHUB_USER)
   return send(res, 200, orderBy(repos, 'stargazers_count', 'desc'))
