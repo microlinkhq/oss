5d99c34dc4e04a62ca79ef6ddbb476e36ea7a84b
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-05 11:32:57 +0200

    build: add memory cache it prevents rate limit for unauthenticated github requests

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  ...process.env,
+  ONE_DAY_SECONDS: 86400,
+  REQ_TIMEOUT: 8000,
+  CACHE: new Map()
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const githubRepositories = require('github-repositories')
+const pTimeout = require('p-timeout')
+const pReflect = require('p-reflect')
 const { orderBy } = require('lodash')
 const { send } = require('micri')
 
-const { GITHUB_USER } = process.env
-
-const ONE_DAY_SECONDS = 86400
+const { REQ_TIMEOUT, CACHE, GITHUB_USER, ONE_DAY_SECONDS } = process.env
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -16,6 +16,13 @@ module.exports = async (req, res) => {
     `public, must-revalidate, max-age=${ONE_DAY_SECONDS}, s-maxage=${ONE_DAY_SECONDS}, stale-while-revalidate=60`
   )
 
-  const repos = await githubRepositories(GITHUB_USER)
-  return send(res, 200, orderBy(repos, 'stargazers_count', 'desc'))
+  const { isFulfilled, value } = await pReflect(
+    pTimeout(githubRepositories(GITHUB_USER), REQ_TIMEOUT)
+  )
+
+  if (isFulfilled) {
+    CACHE.set('payload', orderBy(value, 'stargazers_count', 'desc'))
+  }
+
+  return send(res, 200, CACHE.get('payload'))
 }
